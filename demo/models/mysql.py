06dec62686eff0733cd1bc4f4d7f078d325cd87c
xinghang <xinghang@thunder.com.cn>
2017-01-20 10:45:36 +0800

    update

--- demo/models/mysql.py
+++ demo/models/mysql.py
@@ -4,12 +4,12 @@ __author__ = 'xinghang'
 from peewee import *
 
 mysql_db = MySQLDatabase(
-    host='192.168.0.172',
-    port=3308,
-    database='myktv',
-    user="mombaby",
-    passwd="098f6bcd4621d373cade4e832627b4f6",
-    charset='utf8'
+    host='',
+    port='',
+    database='',
+    user="",
+    passwd="",
+    charset=''
 )
 
 
